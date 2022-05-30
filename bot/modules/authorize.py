b1589f29b6bc29f9ee6155aa81388adf8503a361
Haruto0307 <skybolt0307@gmail.com>
2022-05-30 22:18:30 +0530

    Authorize: Fixed bugs due to str to int conversion

--- bot/modules/authorize.py
+++ bot/modules/authorize.py
@@ -1,5 +1,3 @@
-from contextvars import Context
-from pyparsing import Regex
 from bot.helper.telegram_helper.message_utils import sendMessage
 from telegram.ext import run_async
 from bot import AUTHORIZED_CHATS, OWNER_ID, dispatcher
@@ -32,7 +30,6 @@ def authorize(update: Update,context):
         msg_args = msg_split[1:]
     if msg_args is not None and len(msg_args) > 0:
         match = re.search(r'^-?[0-9]\d*(\.\d+)?$', msg_args[0])
-    msg = ''
     
     if (match is not None):
         chat_id = int(match.group(0))
@@ -48,7 +45,6 @@ def authorize(update: Update,context):
         # Trying to authorize someone in specific
         user_id = reply_message.from_user.id
         _authorize(user_id, context, update)
-    sendMessage(msg, context.bot, update)
 
 def _unauthorize(id: int, context, update):
     chat_id = str(id)
@@ -93,4 +89,3 @@ unauthorize_handler = CommandHandler(command=BotCommands.UnAuthorizeCommand, cal
                                      filters=CustomFilters.owner_filter)
 dispatcher.add_handler(authorize_handler)
 dispatcher.add_handler(unauthorize_handler)
-
